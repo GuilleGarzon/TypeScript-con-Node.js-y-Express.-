7f087853eee3916d3d6178cf5f8a3fed64ef6682
GuilleGarzon <guillegarzon.gg@gmail.com>
2022-11-10 17:10:39 -0500

    feat: add post

--- build/index.js
+++ build/index.js
@@ -1,27 +1,19 @@
-'use strict'
+"use strict";
 var __importDefault = (this && this.__importDefault) || function (mod) {
-  return (mod && mod.__esModule) ? mod : { 'default': mod }
-}
-Object.defineProperty(exports, '__esModule', { value: true })
-// eslint-disable-next-line camelcase
-const express_1 = __importDefault(require('express')) // ESModules
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express")); // ESModules
 // const express = require('express); -> common js
-// eslint-disable-next-line camelcase
-const diaries_1 = __importDefault(require('./routes/diaries'))
-// eslint-disable-next-line camelcase
-const app = (0, express_1.default)()
-// eslint-disable-next-line camelcase
-app.use(express_1.default.json()) // middleware que transforma la req.body a un json
-const PORT = 3000
-
+const diaries_1 = __importDefault(require("./routes/diaries"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json()); // middleware que transforma la req.body a un json
+const PORT = 8080;
 app.get('/ping', (_req, res) => {
-  console.log('someone pinged here!!!')
-  res.send('pong')
-})
-
-// eslint-disable-next-line camelcase
-app.use('/api/diaries', diaries_1.default)
-
+    console.log('someone pinged here!!!');
+    res.send('pong');
+});
+app.use('/api/diaries', diaries_1.default);
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+    console.log(`Server running on port ${PORT}`);
+});
--- build/services/diaryServices.js
+++ build/services/diaryServices.js
@@ -14,15 +14,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.addEntry = exports.getEntriesWithoutSensitiveInfo = exports.findById = exports.getEntries = void 0;
+exports.addDiary = exports.getEntriesWithoutSensitiveInfo = exports.findById = exports.getEntries = void 0;
 const diaries_json_1 = __importDefault(require("./diaries.json"));
 const diaries = diaries_json_1.default;
 const getEntries = () => diaries;
 exports.getEntries = getEntries;
 const findById = (id) => {
     const entry = diaries.find(d => d.id === id);
-    if (entry !== null) {
-        const restOfDiary = __rest(entry, []);
+    if (entry != null) {
+        const { comment } = entry, restOfDiary = __rest(entry, ["comment"]);
         return restOfDiary;
     }
     return undefined;
@@ -39,5 +39,9 @@ const getEntriesWithoutSensitiveInfo = () => {
     });
 };
 exports.getEntriesWithoutSensitiveInfo = getEntriesWithoutSensitiveInfo;
-const addEntry = () => undefined;
-exports.addEntry = addEntry;
+const addDiary = (newDiaryEntry) => {
+    const newDiary = Object.assign({ id: Math.max(...diaries.map(d => d.id)) + 1 }, newDiaryEntry);
+    diaries.push(newDiary);
+    return newDiary;
+};
+exports.addDiary = addDiary;
