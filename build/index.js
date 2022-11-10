8d5f7d6e46a26ef69776e857be3cbb48842a41b4
GuilleGarzon <guillegarzon.gg@gmail.com>
2022-11-10 16:11:09 -0500

    First Commit

---
+++ build/index.js
@@ -0,0 +1,27 @@
+'use strict'
+var __importDefault = (this && this.__importDefault) || function (mod) {
+  return (mod && mod.__esModule) ? mod : { 'default': mod }
+}
+Object.defineProperty(exports, '__esModule', { value: true })
+// eslint-disable-next-line camelcase
+const express_1 = __importDefault(require('express')) // ESModules
+// const express = require('express); -> common js
+// eslint-disable-next-line camelcase
+const diaries_1 = __importDefault(require('./routes/diaries'))
+// eslint-disable-next-line camelcase
+const app = (0, express_1.default)()
+// eslint-disable-next-line camelcase
+app.use(express_1.default.json()) // middleware que transforma la req.body a un json
+const PORT = 3000
+
+app.get('/ping', (_req, res) => {
+  console.log('someone pinged here!!!')
+  res.send('pong')
+})
+
+// eslint-disable-next-line camelcase
+app.use('/api/diaries', diaries_1.default)
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
